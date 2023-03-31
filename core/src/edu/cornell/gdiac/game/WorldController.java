e1e5029dda82faec48d176ac08b64ca1a5bf8e3b
Nicole Thean <nicole@thean.com>
2023-03-31 17:35:08 -0400

    added fake lab tileset

--- core/src/edu/cornell/gdiac/game/WorldController.java
+++ core/src/edu/cornell/gdiac/game/WorldController.java
@@ -204,7 +204,8 @@ public class WorldController implements Screen {
 		String[] names = {"cat", "jumpingCat","barrier", "rope", "spikes", "button", "flame", "flamethrower", "laser", "laserBeam",
 				"deadCat", "checkpoint", "checkpointActive", "roboMob", "background", "steel", "goal",
 				"flame_anim","checkpoint_anim", "checkpoint_active_anim", "button_anim", "jump_anim",
-				"meow_anim","sit","walk"};
+				"meow_anim","sit","walk",
+				"lab_tileset"};
 
 		for (String n : names){
 			textureRegionAssetMap.put(n, new TextureRegion(directory.getEntry(n, Texture.class)));

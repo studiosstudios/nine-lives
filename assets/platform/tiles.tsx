2f8797bc3939404cb152876ac59cf35dfc3b30c0
Sabrina Li <sabrinatli2002@gmail.com>
2023-04-09 16:07:26 -0400

    testing

---
+++ assets/platform/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="tiles" tilewidth="64" tileheight="64" tilecount="48" columns="12">
+ <image source="tiles.jpg" width="768" height="256"/>
+</tileset>

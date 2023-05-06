f58cc940f62cdf808d501418ab10650fb93921f2
Nicole Thean <nicole@thean.com>
2023-05-06 00:18:13 -0400

    added decor to tiled, added assets

--- level-editor/decor/decor.tsx
+++ level-editor/decor/decor.tsx
@@ -1,2 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="inanimates" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.10.1" name="decor" tilewidth="2048" tileheight="1024" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image width="1024" height="1024" source="tutorial-burn.png"/>
+ </tile>
+ <tile id="9">
+  <image width="1024" height="1024" source="tutorial-camera.png"/>
+ </tile>
+ <tile id="10">
+  <image width="2048" height="1024" source="tutorial-checkpoint.png"/>
+ </tile>
+ <tile id="11">
+  <image width="1024" height="1024" source="tutorial-dash.png"/>
+ </tile>
+ <tile id="12">
+  <image width="1024" height="1024" source="tutorial-pause.png"/>
+ </tile>
+ <tile id="13">
+  <image width="2048" height="1024" source="tutorial-spike.png"/>
+ </tile>
+ <tile id="14">
+  <image width="2048" height="1024" source="tutorial-switch.png"/>
+ </tile>
+ <tile id="15">
+  <image width="2048" height="1024" source="tutorial-walk-jump.png"/>
+ </tile>
+</tileset>

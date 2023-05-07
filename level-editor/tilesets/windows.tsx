0905db716883902ff5110629edbba9b20ab4e207
Jeff Zhou <jffzhou@gmail.com>
2023-05-07 00:59:08 -0400

    terrain brushes

--- level-editor/tilesets/windows.tsx
+++ level-editor/tilesets/windows.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="windows" tilewidth="1024" tileheight="1024" tilecount="16" columns="4">
  <image source="window.png" width="4096" height="4096"/>
+ <wangsets>
+  <wangset name="windows" type="edge" tile="-1">
+   <wangcolor name="windows" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="4" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="5" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="6" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="7" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="8" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="9" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="10" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="11" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="12" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="13" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="14" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="15" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
 </tileset>

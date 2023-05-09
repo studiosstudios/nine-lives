5121b511f3d99a1c7da890ea696825dca9b79947
kimmylin727 <kimmylin727@gmail.com>
2023-05-09 01:40:28 -0400

    basic body switching level

--- core/src/edu/cornell/gdiac/game/NineLives.java
+++ core/src/edu/cornell/gdiac/game/NineLives.java
@@ -22,7 +22,7 @@ public class NineLives extends Game implements ScreenListener {
 	private StageController menu;
 	/** The WorldController that contains all LevelControllers*/
 	private GameController controller;
-	private final int TOTAL_LEVELS = 6;
+	private final int TOTAL_LEVELS = 7;
 	private boolean quickLaunchFromTiled;
 	private String filepath;
 
